c5e392322916696efc85dfc20a5f07f6ee266563
sanket17 <sankettikam17@gmail.com>
2024-02-06 05:15:18 -0500

    Capstone - Authentication and authorization added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -29,29 +29,91 @@ def contact(request):
     return render(request,'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/login.html', context)
+    else:
+        return render(request, 'djangoapp/login.html', context)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
+        context = {}
+        # url = "https://sankettikam1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # dealerships = get_dealers_from_cf(url)
+        # context['dealerships'] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
+# Create a `get_dealer_details` view to render tdealershipshe reviews of a dealer
+def get_dealer_details(request, dealer_id):
+     if request.method == "GET":
+        context = {}
+        # Get dealers from the URL
+        url = "https://sankettikam1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        dealer = get_dealer_by_id_from_cf(url, dealer_id)
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+        url = "https://sankettikam1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/reviews/get"
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+        context['reviews'] = reviews
+        context['dealer'] = dealer
+        return render(request, 'djangoapp/dealer_details.html', context)
 
+        
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
-
+def add_review(request, dealer_id):
+    if request.method == "POST":
+        review = dict()
+        review["time"] = datetime.utcnow().isoformat()
+        review["dealership"] = dealer_id
+        review["review"] = request.POST.get('review', '')
+        review["purchase"] = request.POST.get('purchase', '')
+        review["purchase_date"] = request.POST.get('purchase_date')
+        review["name"] = request.POST.get('name', '')
+        review["car_make"] = request.POST.get('car_make')
+        review["car_model"] = request.POST.get('car_model')
+        review["car_year"] = request.POST.get('car_year')
+        url = "https://sankettikam1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/reviews/post"
+        res = post_request(url, review)
+        return redirect("djangoapp:dealer_details", dealer_id=dealer_id)
+    url = "https://sankettikam1-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+    dealer = get_dealer_by_id_from_cf(url, dealer_id)
+    cars = CarModel.objects.all()
+    return render(request, 'djangoapp/add_review.html', {"dealer": dealer, "cars": cars})
